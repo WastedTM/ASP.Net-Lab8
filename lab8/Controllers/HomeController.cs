69c38aa1981819c5517fd7dbef4be67698011d8c
WastedTM <kilivnika62@gmail.com>
2023-10-30 17:07:57 +0200

    final commit lab8

---
+++ lab8/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using lab8.Models;
+
+namespace lab8.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        List<Product> products = new List<Product>
+        {
+            new Product(1, "Product 1", 10.99, DateTime.Now),
+            new Product(2, "Product 2", 19.99, DateTime.Now),
+            new Product(3, "Product 3", 5.99, DateTime.Now)
+        };
+
+        return View(products);
+    }
+}
---
+++ lab8/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿namespace lab8.Models;
+
+public class Product
+{
+    public Product(int id, string name, double price, DateTime createDate)
+    {
+        Id = id;
+        Name = name;
+        Price = price;
+        CreateDate = createDate;
+    }
+
+    public int Id { set; get; }
+    public string Name { set; get; }
+    public double Price { set; get; }
+    public DateTime CreateDate { set; get; }
+    
+    
+}
